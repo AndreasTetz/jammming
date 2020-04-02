dda5f1b92d349c78b04243c3874744b0fab4421a
Andreas Tetz <16765871+AndreasTetz@users.noreply.github.com>
2020-04-02 22:59:03 +0200

    added functions for searchbar and playlist

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -87,15 +87,38 @@ class App extends React.Component {
         this.setState({ playlistTracks: filteredTracks });
     }
 
+    updatePlaylistName = name => {
+        this.setState({ playlistName: name });
+    }
+
+    savePlaylist = () => {
+        let trackURIs = this.state.playlistTracks.map(track => track.uri);
+    }
+
+    search = searchTerm => {
+        console.log(searchTerm);
+    }
+
     render() {
         return (
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar 
+                    onSearch={this.search}
+                    />
                     <div className="App-playlist">
-                        <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-                        <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+                        <SearchResults
+                            searchResults={this.state.searchResults}
+                            onAdd={this.addTrack}
+                        />
+                        <PlayList
+                            playlistName={this.state.playlistName}
+                            playlistTracks={this.state.playlistTracks}
+                            onRemove={this.removeTrack}
+                            onNameChange={this.updatePlaylistName}
+                            onSave={this.savePlaylist}
+                        />
                     </div>
                 </div>
             </div>
--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -2,10 +2,26 @@ import React from "react";
 import "./searchbar.styles.css";
 
 class SearchBar extends React.Component {
+constructor(props) {
+    super(props)
+
+    this.state = {
+        term: ""
+    }
+}
+
+search = () =>{
+    this.props.onSearch(this.state.term)
+}
+
+handleChange = event => {
+    this.setState({ term: event.target.value })
+}
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleChange} />
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
